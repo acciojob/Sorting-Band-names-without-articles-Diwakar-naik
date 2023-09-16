8f6408d1ec008a6420c768b20f4b71dfaa8341f9
Diwakar-naik <137063932+Diwakar-naik@users.noreply.github.com>
2023-09-16 13:12:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+let diwa=["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
+let empty=[];
+for(let i=0;i<diwa.length;i++){
+	let naik=diwa[i].replace("The","").trim();
+	empty.push(naik);
+}
+empty.sort();
+conole.log(empty);
+
 
