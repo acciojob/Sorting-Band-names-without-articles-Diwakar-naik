58803a10fc263273d06f797bf4109543c89eee1f
Diwakar-naik <137063932+Diwakar-naik@users.noreply.github.com>
2023-09-16 13:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your code here
 let reg=/\bthe\b|\ban\b|\ba\b/gi;
+
 let diwa=["The Virupaksha Temple", "an Victoria Memorial", "a Tajmahal"];
+console.log(reg);
 let empty=[];
 for(let i=0;i<diwa.length;i++){
 	let naik=diwa[i].replace("reg","").trim();
